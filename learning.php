43f2f8103d7e92d0465a725c9268bb8607b2aa9d
Christopher Jerome Herrera <Christopherjeromeherrera@gmail.com>
2018-01-11 17:28:53 -0700

    upload to server

---
+++ learning.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+<body>
+    <header>
+    <h1> This is my learning area</h1>
+    </header>
+    <section>
+
+<p>Today I began to read CH 3. Intro to PHP</p>
+<ul>
+    <li><em>Topics</em><br>Basic Syntax, Variables and Arrays.</li>
+</ul>
+        </section>
+<div>
+    <p>I am going to continue my work tomorrow with more hands on learning using PHP and start cleaning up my Github</p>
+</div>
+
+<img src="Git%20Fire.jpg">
+
+    <footer>
+        Built by CJH Inc.
+    </footer>
+<p>Click below for a good laugh!!! </p>
+    <a href="https://www.youtube.com/watch?v=wXQUhX89vtQ">Meow</a>
+
+</body>
+</html>
+
+
